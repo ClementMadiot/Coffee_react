4ef4147793082e729f8f8c11964194f55de04b2b
ClementMadiot <clementmadiot09@gmail.com>
2024-09-11 15:28:22 +0200

    add content nav

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="absolute top-0 left-0 w-full pt-10 text-white z-20">
+      <article className="container">
+        <div className="flex justify-between items-center">
+          {/* logo section  */}
+          <h1 className="text-2xl font-semibold text-primary uppercase">
+            Coders <span className="text-white">Coffee.</span>
+          </h1>
+          {/* Hamburger  */}
+          <div>
+            <GiHamburgerMenu className="text-3xl cursor-pointer"/>
+          </div>
+        </div>
+      </article>
+    </nav>
+  );
 };
 
 export default Navbar;
