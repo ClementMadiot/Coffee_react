77eb66def2d2ffd6ce7fb4905c0638e2ad2b6934
ClementMadiot <clementmadiot09@gmail.com>
2024-09-11 15:57:16 +0200

    add framer motion to Nav + Hero

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,19 +1,39 @@
 import React from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   return (
-    <nav className="absolute top-0 left-0 w-full pt-10 text-white z-20">
+    <nav className="absolute top-0 left-0 w-full pt-10 text-white z-40">
       <article className="container">
         <div className="flex justify-between items-center">
           {/* logo section  */}
-          <h1 className="text-2xl font-semibold text-primary uppercase">
+          <motion.h1
+            initial={{ opacity: 0, y: -100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{
+              type: "spring",
+              stiffness: 100,
+              damping: 10,
+              delay: 0.2,
+            }}
+            className="text-2xl font-semibold text-primary uppercase"
+          >
             Coders <span className="text-white">Coffee.</span>
-          </h1>
+          </motion.h1>
           {/* Hamburger  */}
-          <div>
-            <GiHamburgerMenu className="text-3xl cursor-pointer"/>
-          </div>
+          <motion.div
+            initial={{ opacity: 0, y: -100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{
+              type: "spring",
+              stiffness: 100,
+              damping: 10,
+              delay: 0.2,
+            }}
+          >
+            <GiHamburgerMenu className="text-3xl cursor-pointer" />
+          </motion.div>
         </div>
       </article>
     </nav>
