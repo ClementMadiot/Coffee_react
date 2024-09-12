bbf6779ea48c34f3c0e678bdaa1ed979130cb5c0
ClementMadiot <clementmadiot09@gmail.com>
2024-09-12 17:26:27 +0200

    add serviceData to constant/index.jsx

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <section className="container my-16 space-y-4">
+      {/* header section  */}
+      <div className=" text-center max-w-lg mx-auto space-y-2">
+        <motion.h1
+          initial={{ opacity: 0, y: 100 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{
+            type: "spring",
+            stiffness: 100,
+            damping: 11,
+            delay: 0.5,
+          }}
+          className=" text-3xl font-bold text-lightGray mb-4"
+        >
+          Fresh and <span className="text-primary">Tasty coffee</span>
+        </motion.h1>
+        <motion.p
+          initial={{ opacity: 0, scale: 0.5 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          transition={{
+            type: "spring",
+            stiffness: 100,
+            damping: 11,
+            delay: 0.6,
+          }}
+          className="text-sm opacity-80"
+        >
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos
+          reprehenderit eum ducimus, distinctio cum eaque totam voluptatem ex
+          dolorem fuga.
+        </motion.p>
+      </div>
+      {/* card section  */}
+    </section>
+  );
 };
 
 export default Services;
---
+++ src/constant/index.jsx
@@ -0,0 +1,23 @@
+import { Coffee1, Coffee3 } from "../assets/index"
+
+
+export const servicesData = [
+  {
+    id: 1,
+    image: {Coffee1},
+    title: 'Black Coffee',
+    subtitle: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    },
+    {
+    id: 2,
+    image: {Coffee3},
+    title: 'Hot Coffee',
+    subtitle: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    },
+    {
+    id: 3,
+    image: {Coffee1},
+    title: 'Cold Coffee',
+    subtitle: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+  }
+]
